9e099581dabdaaca29abf960b8da01961d3b4ebb
Davida Gaffney <gdavida@gmail.com>
2016-06-01 14:51:33 -0500

    location controller tests all working

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,22 +1,62 @@
+# class CreateLocations < ActiveRecord::Migration
+#   def change
+#     create_table :locations do |t|
+#       t.string :branch_name, null: false
+#       t.string :address
+#       t.string :contact_name
+
+#       t.timestamps null: false
+#     end
+#   end
+# end
+
+
 class LocationsController < ApplicationController
   def index
+    @locations = Location.all
   end
 
   def create
+    @location = Location.new(location_params)
+
+    if@location.save
+      redirect_to :locations, notice: "Success created"
+    else
+      flash.now[:alert] = "Couldn't create"
+      render :new
+    end
   end
 
   def new
+    @location = Location.new
   end
 
   def edit
+    @location = Location.find_by_id(params[:id])
   end
 
   def show
+    @location = Location.find_by_id(params[:id])
   end
 
   def update
+    @location = Location.find_by_id(params[:id])
+    if@location.update_attributes(location_params)
+      redirect_to location_path(@location), notice: "Successfully updated"
+    else
+      flash.now[:alert] = "Couldn't Update"
+      render :new
+    end
   end
 
   def destroy
+    @location = Location.find_by_id(params[:id])
+    @location.destroy
+    redirect_to :locations, notice: "Successfully destroyed!"
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(:branch_name, :address, :contact_name)
   end
 end
--- test/controllers/locations_controller_test.rb
+++ test/controllers/locations_controller_test.rb
@@ -1,39 +1,39 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class LocationsControllerTest < ActionController::TestCase
-#   test "should get index" do
-#     get :index
-#     assert_response :success
-#   end
+class LocationsControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
 
-#   test "should get create" do
-#     get :create
-#     assert_response :success
-#   end
+  test "should get create" do
+    post :create, location:locations(:one).attributes.except("id")
+    assert_redirected_to locations_path
+  end
 
-#   test "should get new" do
-#     get :new
-#     assert_response :success
-#   end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 
-#   test "should get edit" do
-#     get :edit
-#     assert_response :success
-#   end
+  test "should get edit" do
+    get :edit, id:locations(:one)
+    assert_response :success
+  end
 
-#   test "should get show" do
-#     get :show
-#     assert_response :success
-#   end
+  test "should get show" do
+    get :show, id:locations(:one)
+    assert_response :success
+  end
 
-#   test "should get update" do
-#     get :update
-#     assert_response :success
-#   end
+  test "should get update" do
+    patch :update, id:locations(:one), location:{name:"Something New"}
+    assert_redirected_to location_path(locations(:one))
+  end
 
-#   test "should get destroy" do
-#     get :destroy
-#     assert_response :success
-#   end
+  test "should get destroy" do
+    get :destroy, {'id' => locations(:one).id}
+    assert_redirected_to locations_path
+  end
 
-# end
+end
