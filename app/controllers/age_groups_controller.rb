ce3c49f06fddd9c3117f2f54e239206014c4c1e1
Davida Gaffney <gdavida@gmail.com>
2016-06-01 15:01:42 -0500

    age group controller tests all passing

--- app/controllers/age_groups_controller.rb
+++ app/controllers/age_groups_controller.rb
@@ -1,22 +1,61 @@
+# class CreateAgeGroups < ActiveRecord::Migration
+#   def change
+#     create_table :age_groups do |t|
+#       t.string :age,  null: false
+#       t.string :color,  null: false
+
+#       t.timestamps null: false
+#     end
+#   end
+# end
+
+
 class AgeGroupsController < ApplicationController
   def index
+    @age_groups = AgeGroup.all
   end
 
   def create
+    @age_group = AgeGroup.new(age_group_params)
+
+    if@age_group.save
+      redirect_to :age_groups, notice: "Success created"
+    else
+      flash.now[:alert] = "Couldn't create"
+      render :new
+    end
   end
 
   def new
+    @age_group = AgeGroup.new
   end
 
   def edit
+    @age_group = AgeGroup.find_by_id(params[:id])
   end
 
   def show
+    @age_group = AgeGroup.find_by_id(params[:id])
   end
 
   def update
+    @age_group = AgeGroup.find_by_id(params[:id])
+    if@age_group.update_attributes(age_group_params)
+      redirect_to age_group_path(@age_group), notice: "Successfully updated"
+    else
+      flash.now[:alert] = "Couldn't Update"
+      render :new
+    end
   end
 
   def destroy
+    @age_group = AgeGroup.find_by_id(params[:id])
+    @age_group.destroy
+    redirect_to :age_groups, notice: "Successfully destroyed!"
+  end
+
+  private
+  def age_group_params
+    params.require(:age_group).permit(:age, :color)
   end
 end
--- test/controllers/age_groups_controller_test.rb
+++ test/controllers/age_groups_controller_test.rb
@@ -1,39 +1,39 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class AgeGroupsControllerTest < ActionController::TestCase
-#   test "should get index" do
-#     get :index
-#     assert_response :success
-#   end
+class AgeGroupsControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
 
-#   test "should get create" do
-#     get :create
-#     assert_response :success
-#   end
+  test "should get create" do
+    post :create, age_group:age_groups(:one).attributes.except("id")
+    assert_redirected_to age_groups_path
+  end
 
-#   test "should get new" do
-#     get :new
-#     assert_response :success
-#   end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 
-#   test "should get edit" do
-#     get :edit
-#     assert_response :success
-#   end
+  test "should get edit" do
+    get :edit, id:age_groups(:one)
+    assert_response :success
+  end
 
-#   test "should get show" do
-#     get :show
-#     assert_response :success
-#   end
+  test "should get show" do
+    get :show, id:age_groups(:one)
+    assert_response :success
+  end
 
-#   test "should get update" do
-#     get :update
-#     assert_response :success
-#   end
+  test "should get update" do
+    patch :update, id:age_groups(:one), age_group:{name:"Something New"}
+    assert_redirected_to age_group_path(age_groups(:one))
+  end
 
-#   test "should get destroy" do
-#     get :destroy
-#     assert_response :success
-#   end
+  test "should get destroy" do
+    get :destroy, {'id' => age_groups(:one).id}
+    assert_redirected_to age_groups_path
+  end
 
-# end
+end
