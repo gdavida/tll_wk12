847b21a5fbf94c375fe9e5483e5765c4dbd081c4
Davida Gaffney <gdavida@gmail.com>
2016-06-01 14:34:27 -0500

    boxes controllers all pass

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,22 +1,94 @@
+# class CreateBoxes < ActiveRecord::Migration
+#   def change
+#     create_table :boxes do |t|
+#       t.string :name, null: false
+#       t.integer :age_group_id
+#       t.integer :status_id, null: false
+#       t.integer :user_id
+#       t.integer :location_id, null: false
+
+#       t.timestamps null: false
+#     end
+#   end
+# end
+
+
 class BoxesController < ApplicationController
   def index
+    @boxes = Box.all
   end
 
   def create
+    @box = Box.new(box_params)
+    @age_groups = AgeGroup.all
+    @age_group = AgeGroup.find_by_id(params[:id])
+    @statuses = Status.all
+    @status = Status.find_by_id(params[:id])
+    @locations = Location.all
+    @location = Location.find_by_id(params[:id])
+    # @users = User.all
+    # @user = User.find_by_id(params[:id])
+
+    if@box.save
+      redirect_to :boxes, notice: "Success created"
+    else
+      flash.now[:alert] = "Couldn't create"
+      render :new
+    end
   end
 
   def new
+    @box = Box.new
+    @age_groups = AgeGroup.all
+    @age_group = AgeGroup.find_by_id(params[:id])
+    @age_groups = AgeGroup.all
+    @age_group = AgeGroup.find_by_id(params[:id])
+    @statuses = Status.all
+    @status = Status.find_by_id(params[:id])
+    @locations = Location.all
+    @location = Location.find_by_id(params[:id])
+    # @users = User.all
+    # @user = User.find_by_id(params[:id])
+
   end
 
   def edit
+    @box = Box.find_by_id(params[:id])
   end
 
   def show
+    @box = Box.find_by_id(params[:id])
+    @age_groups = AgeGroup.all
+    @age_group = @box.age_group
   end
 
   def update
+    @box = Box.find_by_id(params[:id])
+    @age_groups = AgeGroup.all
+    @age_group = AgeGroup.find_by_id(params[:id])
+    @statuses = Status.all
+    @status = Status.find_by_id(params[:id])
+    @locations = Location.all
+    @location = Location.find_by_id(params[:id])
+    # @users = User.all
+    # @user = User.find_by_id(params[:id])
+
+    if@box.update_attributes(box_params)
+      redirect_to box_path(@box), notice: "Successfully updated"
+    else
+      flash.now[:alert] = "Couldn't Update"
+      render :new
+    end
   end
 
   def destroy
+    @box = Box.find_by_id(params[:id])
+    @box.destroy
+    redirect_to :boxes, notice: "Successfully destroyed!"
+  end
+
+  private
+  def box_params
+    params.require(:box).permit(:name, :age_group_id, :status_id, :user_id, :location_id)
   end
 end
--- test/controllers/boxes_controller_test.rb
+++ test/controllers/boxes_controller_test.rb
@@ -1,39 +1,39 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class BoxesControllerTest < ActionController::TestCase
-#   test "should get index" do
-#     get :index
-#     assert_response :success
-#   end
+class BoxesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
 
-#   test "should get create" do
-#     get :create
-#     assert_response :success
-#   end
+  test "should get create" do
+    post :create, box:boxes(:one).attributes.except("id")
+    assert_redirected_to boxes_path
+  end
 
-#   test "should get new" do
-#     get :new
-#     assert_response :success
-#   end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 
-#   test "should get edit" do
-#     get :edit
-#     assert_response :success
-#   end
+  test "should get edit" do
+    get :edit, id:boxes(:one)
+    assert_response :success
+  end
 
-#   test "should get show" do
-#     get :show
-#     assert_response :success
-#   end
+  test "should get show" do
+    get :show, id:boxes(:one)
+    assert_response :success
+  end
 
-#   test "should get update" do
-#     get :update
-#     assert_response :success
-#   end
+  test "should get update" do
+    patch :update, id:boxes(:one), box:{name:"Something New"}
+    assert_redirected_to box_path(boxes(:one))
+  end
 
-#   test "should get destroy" do
-#     get :destroy
-#     assert_response :success
-#   end
+  test "should get destroy" do
+    get :destroy, {'id' => boxes(:one).id}
+    assert_redirected_to boxes_path
+  end
 
-# end
+end
