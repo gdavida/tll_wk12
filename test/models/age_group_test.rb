d901dc0d7d8dd3633541853a139395ac18727ec7
Davida Gaffney <gdavida@gmail.com>
2016-05-31 21:16:37 -0500

    agegroup test not passing

--- test/models/age_group_test.rb
+++ test/models/age_group_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class AgeGroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "test is valid" do
+  	@age_group = age_groups(:one)
+    assert(@age_group.valid?, "This should be valid under normal circcumstances")
+  end
+
+  test "test should fail if age is not present" do
+  	@age_group = age_groups(:two)
+  	@age_group.age = ""
+  	refute(@age_group.valid?, "This should be invalid without a age")
+	end
+
+  test "test should fail if color is not present" do
+  	@age_group = age_groups(:two)
+  	@age_group.color = ""
+  	refute(@age_group.valid?, "This should be invalid without a color")
+	end
 end
